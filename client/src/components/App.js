762f01b7b9fff4ee3de3180fb7c6cd0961342362
chumothyLee <llol5394@gmail.com>
2018-04-23 23:37:01 -0700

    add comments for jsdoc

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,7 @@
+/**
+ * UI for testing functionality of PetersComponent 
+ * @author chumothyLee
+ */
 import React, { Component } from 'react';
 import { Container, Row, Col, Form, Label, Input } from 'reactstrap';
 import { GithubPicker } from 'react-color';
@@ -13,11 +17,23 @@ class App extends Component {
     }
   }
 
+  /**
+   * Updates component's state.color value when user selects a new color on GithubPicker
+   * @public
+   * @param {color} color New color value for PetersComponent. Use hex key.
+   * @returns {null}
+   */
   handleChange = (color) => {
     console.log(color);
     this.setState({ color : color.hex });
   }
 
+  /**
+   * Updates component's state.time value when user inputs numerical value to #timeSelector
+   * @public
+   * @param {*} event Event from which to take update number value
+   * @returns {null}
+   */
   handleTimeChange = (event) => {
     const times = event.target.value;
     console.log(times);
@@ -34,7 +50,7 @@ class App extends Component {
                   <Form>
                     <Label for="colorPicker"> Color of text </Label>
                     <GithubPicker id="colorPicker" color={this.state.color} onChange={this.handleChange}/>
-                    <Label for="timesSelector"> Number of times to print text </Label>
+                    <Label for="timesSelector"> Times to print text </Label>
                     <Input id="timesSelector" type="textarea" onChange={this.handleTimeChange}/>
                   </Form>
                 </Col>
--- client/src/components/PetersComponent.js
+++ client/src/components/PetersComponent.js
@@ -1,8 +1,15 @@
+/**
+ * A basic custom component that renders a {color}-colored string {times} times 
+ * @author chumothyLee
+ */
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
+/**
+ * @param {*} props Props to be passed into component. Namely {times, color} 
+ */
 const PetersComponent = (props) => {
-    console.log("this version automatically linted, tested, built, and deployed");
     console.log(props);
     const petersString = "Look Peter, the component works!".repeat(props.times);
 
@@ -12,7 +19,9 @@ const PetersComponent = (props) => {
 }
 
 PetersComponent.propTypes = {
+    /** number of times to print string */
     times: PropTypes.number,
+    /** color in which to print string */
     color: PropTypes.string
 }
 
